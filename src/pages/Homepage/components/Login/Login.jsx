d559ba6b45db5d23f96640892cbcdfb926a348b7
YuliiaZaliesna <yuliazalesna2002@gmail.com>
2022-07-21 16:38:23 +0300

    Added homepage and components

--- src/pages/Homepage/components/Login/Login.jsx
+++ src/pages/Homepage/components/Login/Login.jsx
@@ -1,6 +1,11 @@
 function Login(){
     return(
-        <div>Login!@!!!!</div>
+        <div>
+            <h1>Zaloguj się</h1>
+            <input type="text" placeholder="Podaj login" />
+            <input type="password" placeholder="Podaj hasło" />
+            <input type="submit" value="Zaloguj" />
+        </div>
     );
 }
 export default  Login;
